881264eeb481b5dec7c68db95e4d91db2f92bc6a
Richard Hoffmann <rjh.hoffmann@googlemail.com>
2018-12-18 13:55:37 +0100

    update card file

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,28 +1,6 @@
 import React from "react";
-
-import CardView from './CardView.bs';
 import "./Card.css";
 
-class Card extends React.Component {
-  state = {
-    flipped: true
-  };
+import CardContainer from './CardContainer.bs';
 
-  flip = () => {
-    this.setState({ flipped: !this.state.flipped });
-  };
-
-  render() {
-    const { code, imageSource } = this.props;
-    return (
-      <CardView
-        code={code}
-        imageSource={imageSource}
-        flipped={this.state.flipped}
-        onClick={this.flip}
-      />
-    )
-  }
-}
-
-export { Card };
+export { CardContainer as Card };
